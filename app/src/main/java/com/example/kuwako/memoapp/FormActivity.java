d124e454b74e3634b1ce465e7880d0585fbb3d11
masaki_kuwako <m.kuwako0702@gmail.com>
2016-05-07 22:25:57 +0900

    オプションメニューに削除と保存作成

--- app/src/main/java/com/example/kuwako/memoapp/FormActivity.java
+++ app/src/main/java/com/example/kuwako/memoapp/FormActivity.java
@@ -9,6 +9,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -78,5 +80,35 @@ public class FormActivity extends AppCompatActivity {
 
             c.close();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                deleteMemo();
+
+                break;
+            case R.id.action_save:
+                saveMemo();
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteMemo() {
+
+    }
+
+    private void saveMemo() {
+
     }
 }
